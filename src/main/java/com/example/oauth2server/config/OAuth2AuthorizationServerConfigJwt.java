742bcbc9e20857bd0875069b328d44fd941431be
phuc.nguyenvan <phucnv9985@yahoo.com>
2019-01-08 17:35:47 +0700

    OAuth2: show login page

--- src/main/java/com/example/oauth2server/config/OAuth2AuthorizationServerConfigJwt.java
+++ src/main/java/com/example/oauth2server/config/OAuth2AuthorizationServerConfigJwt.java
@@ -1,6 +1,8 @@
 package com.example.oauth2server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -13,17 +15,21 @@ public class OAuth2AuthorizationServerConfigJwt extends AuthorizationServerConfi
     public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient("sampleClientId").authorizedGrantTypes("implicit").scopes("read", "write", "foo", "bar").autoApprove(false).accessTokenValiditySeconds(3600).redirectUris("http://localhost:8083/","http://localhost:8086/")
 
-                .and().withClient("fooClientIdPassword").secret(/*passwordEncoder().encode(*/"secret"/*)*/).authorizedGrantTypes("password", "authorization_code", "refresh_token").scopes("foo", "read", "write").accessTokenValiditySeconds(3600)
+                .and().withClient("fooClientIdPassword").secret(passwordEncoder().encode("secret")).authorizedGrantTypes("password", "authorization_code", "refresh_token").scopes("foo", "read", "write").accessTokenValiditySeconds(3600)
                 // 1 hour
                 .refreshTokenValiditySeconds(2592000)
                 // 30 days
                 .redirectUris("xxx","http://localhost:8089/","http://localhost:8080/login/oauth2/code/custom")
 
-                .and().withClient("barClientIdPassword").secret(/*passwordEncoder().encode(*/"secret"/*)*/).authorizedGrantTypes("password", "authorization_code", "refresh_token").scopes("bar", "read", "write").accessTokenValiditySeconds(3600)
+                .and().withClient("barClientIdPassword").secret(passwordEncoder().encode("secret")).authorizedGrantTypes("password", "authorization_code", "refresh_token").scopes("bar", "read", "write").accessTokenValiditySeconds(3600)
                 // 1 hour
                 .refreshTokenValiditySeconds(2592000) // 30 days
 
                 .and().withClient("testImplicitClientId").authorizedGrantTypes("implicit").scopes("read", "write", "foo", "bar").autoApprove(true).redirectUris("xxx");
 
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/oauth2server/config/SecurityConfig.java
+++ src/main/java/com/example/oauth2server/config/SecurityConfig.java
@@ -1,41 +1,54 @@
 package com.example.oauth2server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                    .disable()
-                .authorizeRequests()
-                    .anyRequest().permitAll()
-                .and()
-                    .httpBasic()
-                .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
+				/*
+				 * allow any user to access the main page, the login page, and the resources
+				 * pages
+				 */
+				.antMatchers("/", "/login", "/resources/**").permitAll()
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                    .password("password1")
-                    .roles("USER")
-            .and()
-                .withUser("manager")
-                    .password("password")/*
-                    .credentialsExpired(true)
-                    .accountExpired(true)
-                    .accountLocked(false)*/
-                    .authorities("WRITE_PRIVILEGES", "READ_PRIVILEGES")
-                    .roles("MANAGER");
-    }
+				/*
+				 * require that any user who access vendor pages should have a role
+				 * "ROLE_VENDOR"
+				 */
+				.antMatchers("/vendor/**").hasRole("VENDOR")
 
+				/*
+				 * configure that a user with either role "ROLE_CUSTOMER" or "ROLE_VENDOR" will
+				 * have access to the customer pages
+				 */
+				.antMatchers("/customer/**").hasAnyRole("CUSTOMER, VENDOR")
+
+				.and()
+
+				/*
+				 * automatically generate the log in page
+				 */
+				.formLogin();
+	}
+    @Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				/*
+				 * configure user "vendor" with password "vendor" and role "ROLE_VENDOR"
+				 */
+				.withUser("vendor").password("vendor").roles("VENDOR").and()
+				/*
+				 * configure user "customer" with password "customer" and role "ROLE_CUSTOMER"
+				 */
+				.withUser("customer").password("customer").roles("CUSTOMER");
+	}
 }
